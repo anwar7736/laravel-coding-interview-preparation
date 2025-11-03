378b53c19bd3f27830f881365b1764605d0d3530
Md Anwar Hossain <anwarhossain7736@gmail.com>
2025-11-03 12:36:42 +0600

    Implemented on multi step form with bootstrap and jquery

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,15 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+
+Route::get('/multi-step-form', [App\Http\Controllers\MultiStepFormController::class, 'create'])->name('create');
+Route::post('/stepOne', [App\Http\Controllers\MultiStepFormController::class, 'stepOne'])->name('stepOne');
+Route::post('/stepTwo', [App\Http\Controllers\MultiStepFormController::class, 'stepTwo'])->name('stepTwo');
+Route::post('/store', [App\Http\Controllers\MultiStepFormController::class, 'store'])->name('store');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
