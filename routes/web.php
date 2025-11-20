dc4a4cc147761e4eb2140c78a1c0a6a0ac9e6d31
anwar7736 <anwarhossain7736@gmail.com>
2025-11-20 22:08:48 +0600

    Something changes in laravel.yml file

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SQLExecuteController;
+use App\Modules\Product\Models\Brand;
+use Illuminate\Support\Facades\DB;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -28,4 +30,37 @@ Route::group([
 ], function () {
         Route::get('/create', 'create')->name('create');
     Route::post('/execute', 'execute')->name('execute');
+});
+
+Route::group(['prefix' => 'database-replication', 'as' => 'database-replication'], function(){
+        Route::get('list-brands', function(){
+                // dd(DB::select("SELECT @@port as port"));
+                $brands = Brand::active()
+                ->select(
+                        'id',
+                        'name',
+                        'short_description',
+                        DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d %h:%i') as created_date")
+                )
+                ->latest('id')
+                ->get();
+
+                return response([
+                  'success' => true,
+                  'data'    => $brands
+                ]);
+        });
+
+        Route::get('add-brand', function(){
+                $brand = Brand::create([
+                        'name' => 'MY NEW TEST BRAND '.rand(111111,999999),
+                        ''
+                ]);
+                
+                return response([
+                  'success' => true,
+                  'message' => "New brand added successfully",
+                  'data'    => $brand
+                ]);
+        });
 });
